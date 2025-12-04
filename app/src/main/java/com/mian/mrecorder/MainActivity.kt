2cf460cd575807fa8758e366f3fb414431596cb9
mianxzumer <mianxzumer@gmail.com>
2025-12-05 02:33:46 +0500

    Add MainActivity for MRecorder app

---
+++ app/src/main/java/com/mian/mrecorder/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.mian.mrecorder
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.mian.mrecorder.ui.RecorderScreen
+import com.mian.mrecorder.ui.theme.MRecorderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MRecorderTheme {
+                RecorderScreen()
+            }
+        }
+    }
+}
